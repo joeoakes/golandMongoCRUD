56e8249356ac74216a12d64c3c3294c9c231cbf8
joeoakes <jxo19@psu.edu>
2023-10-29 13:27:51 -0400

    commit

--- main.go
+++ main.go
@@ -1,52 +1,92 @@
 package main
 
 import (
-	"database/sql"
+	"context"
 	"fmt"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 	"log"
 )
 
 func main() {
-	// Open a new database connection
-	db, err := sql.Open("mysql", "username:password@tcp(127.0.0.1:3306)/dbname")
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer db.Close()
+	// Set client options
+	clientOptions := options.Client().ApplyURI("mongodb://localhost:27017")
 
-	// CREATE operation
-	_, err = db.Exec("INSERT INTO users(name, age) VALUES (?, ?)", "John Doe", 30)
-	if err != nil {
-		log.Fatal(err)
-	}
+	// Connect to MongoDB
+	client, err := mongo.Connect(context.TODO(), clientOptions)
 
-	// READ operation
-	rows, err := db.Query("SELECT id, name, age FROM users")
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer rows.Close()
-
-	for rows.Next() {
-		var id int
-		var name string
-		var age int
-		err := rows.Scan(&id, &name, &age)
-		if err != nil {
-			log.Fatal(err)
-		}
-		fmt.Println(id, name, age)
-	}
-
-	// UPDATE operation
-	_, err = db.Exec("UPDATE users SET age = ? WHERE name = ?", 31, "John Doe")
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	// DELETE operation
-	_, err = db.Exec("DELETE FROM users WHERE name = ?", "John Doe")
+	// Check the connection
+	err = client.Ping(context.TODO(), nil)
+
 	if err != nil {
 		log.Fatal(err)
 	}
+
+	fmt.Println("Connected to MongoDB!")
+
+	// Get a handle for your collection
+	collection := client.Database("test").Collection("users")
+
+	//Create (Insert Documents)
+	user := map[string]interface{}{
+		"name": "John Doe",
+		"age":  30,
+	}
+
+	insertResult, err := collection.InsertOne(context.TODO(), user)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Inserted a Single Document: ", insertResult.InsertedID)
+
+	//Read (Find Documents)
+	// Find a single document
+	var result map[string]interface{}
+	err = collection.FindOne(context.TODO(), map[string]interface{}{"name": "John Doe"}).Decode(&result)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Found a single document: ", result)
+
+	//Update Documents
+	filter := map[string]interface{}{"name": "John Doe"}
+	update := map[string]interface{}{
+		"$set": map[string]interface{}{
+			"age": 31,
+		},
+	}
+
+	updateResult, err := collection.UpdateOne(context.TODO(), filter, update)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("Matched %v documents and updated %v documents.\n", updateResult.MatchedCount, updateResult.ModifiedCount)
+
+	//Delete document
+	deleteResult, err := collection.DeleteOne(context.TODO(), map[string]interface{}{"name": "John Doe"})
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("Deleted %v documents in the trainers collection\n", deleteResult.DeletedCount)
+
+	//Disconnect from MongoDb
+	err = client.Disconnect(context.TODO())
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("Connection to MongoDB closed.")
+
 }
